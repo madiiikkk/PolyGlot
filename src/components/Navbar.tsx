ecc20b43f35afbf2efa14f11446b8fe9875127d8
madiiikkk <madik2212@gmail.com>
2025-02-06 02:20:07 +0500

    убрал фон лого

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import HomePage from '../pages/HomePage'
 import '../styles/Navbar.css'
 
 function Navbar() {
   return (
     <div>
         <nav className='navbar'>
-            <div className=''>
+            <div className='links'>
                 <Link to="/" className="logo">
+                    PolyGlot
+                </Link>
+            </div>
+            <div className='links'>
+                <Link to="/" className="link">
+                    PolyGlot
+                </Link>
+            </div>
+            <div className='links'>
+                <Link to="/" className="link">
+                    PolyGlot
+                </Link>
+            </div>
+            <div className='links'>
+                <Link to="/" className="link">
                     PolyGlot
                 </Link>
             </div>
